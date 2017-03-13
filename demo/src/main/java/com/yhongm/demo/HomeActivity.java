650fab39cf85355ff0ac0f0fb482320fa0eeb7f1
yhongm <869429751@qq.com>
2017-03-13 18:08:41 +0800

    change

--- demo/src/main/java/com/yhongm/demo/HomeActivity.java
+++ demo/src/main/java/com/yhongm/demo/HomeActivity.java
@@ -27,34 +27,6 @@ public class HomeActivity extends Activity {
         TestBean response = execute.getResponse();
         Log.i("HomeActivity", "17:37/onCreate:str,testBean:" + response.toString());// yhongm 2017/03/09 17:37
         Log.i("HomeActivity", "15:42/onCreate:testBeanSize:" + response.getNewslist().size());// yhongm 2017/03/13 15:42
-//        new Thread(new Runnable() {
-//            @Override
-//            public void run() {
-//                Retrofit retrofit = new Retrofit.Builder()
-//                        .baseUrl(RequestInterface.HOST)
-//                        .addConverterFactory(GsonConverterFactory.create())
-//                        .build();
-//                HashMap<String, String> strs = new HashMap<String, String>();
-//                for (int i = 0; i < 10; i++) {
-//                    strs.put(i + "", (10 - i) + "");
-//                }
-//                RequestInterface anInterface = retrofit.create(RequestInterface.class);
-//                ArrayList<Integer> integers = new ArrayList<Integer>();
-//                integers.add(123);
-//                integers.add(999);
-//                integers.add(909);
-//                retrofit.Call<WeiXinItemBean> call = anInterface.getWXHot("bf10ecf0e08cf0796862ccd1b42aa738", 20,
-//                        1);
-//                try {
-//                    retrofit.Response<WeiXinItemBean> execute = call.execute();
-//                    WeiXinItemBean body = execute.body();
-//                    Log.i("HomeActivity", "16:44/onCreate:wxHot:" + body.toString());// yhongm 2017/03/09 16:44
-//                } catch (IOException e) {
-//                    Log.i("HomeActivity", "16:46/onCreate:e:" + e.toString());// yhongm 2017/03/09 16:46
-//                    e.printStackTrace();
-//                }
-//            }
-//        }).start();
     }
 
     public HashMap<String, Integer> test(HashMap<String, Integer> hms) {
